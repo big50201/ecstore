1d1aec4df5509b388908575336b6381413aa7b99
jennifer.sung <jennifer.sung@skylinetw.com>
2019-01-30 17:34:31 +0800

    private checkout path

--- client/src/index.js
+++ client/src/index.js
@@ -1,15 +1,28 @@
-import React from 'react';
+import React,{Component} from 'react';
 import ReactDOM from 'react-dom';
 // import './index.css';
 import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
-import {BrowserRouter as Router,Switch,Route} from 'react-router-dom';
+import {BrowserRouter as Router,Switch,Route,Redirect} from 'react-router-dom';
 import Signin from './components/Signin';
 import Signup from './components/Signup';
 import Checkout from './components/Checkout';
 import 'gestalt/dist/gestalt.css';
 import Navbar from './components/Navbar';
 import Brews from './components/Brews';
+import {getTokenKey} from './utils';
+
+const PrivateRoute = ({component:Component,...rest})=>(
+    <Route {...rest} render={props=>(getTokenKey() !==null ? 
+        <Component {...props}/>:
+        <Redirect to={{
+            pathname:"/signin",
+            state:{from:props.location}
+        }}
+        />)
+        }
+    />
+)
 const Root = ()=>(
     <Router>
         <React.Fragment>
@@ -18,7 +31,7 @@ const Root = ()=>(
             <Route component={App} exact path="/"/>
             <Route component={Signin} path="/signin"/>
             <Route component={Signup} path="/signup"/>
-            <Route component={Checkout} path="/checkout"/>
+            <PrivateRoute component={Checkout} path="/checkout"/>
             <Route component={Brews} path="/:brandId"/>
         </Switch>
         </React.Fragment>
